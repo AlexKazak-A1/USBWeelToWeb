ffc720f28f20e3f5af90addc81711a157c33d543
Alexander Kazakevich <a.kazakevich@a1.by>
2024-09-20 12:23:21 +0300

    added support for DigitalInput devices

--- USBWeelToWeb/Program.cs
+++ USBWeelToWeb/Program.cs
@@ -1,18 +1,99 @@
 ﻿using Vortice.XInput;
 
-namespace USBWeelToWeb
+namespace USBWeelToWeb;
+
+internal class Program
 {
-    internal class Program
+
+    private static void ClearLine()
+    {
+        Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+    }
+
+    static void Main(string[] args)
     {
 
-        private static void ClearLine()
+
+        DIInputDevice directInputDevice = new DIInputDevice();
+        directInputDevice.Initialize(IntPtr.Zero);
+
+
+        int userIndex = 0;
+        int[] searchArray = new int[4] { -1, -1, -1, -1 };
+
+        while (true)
         {
-            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+            if (Console.KeyAvailable)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'q':
+                        return;
+
+                    case 'x':
+                        XInputSelect(searchArray.Where(x => x >=0).ToArray());
+                        break;
+
+                    case 'd':
+                        directInputDevice.GetKJoystickUpdates();
+                        break;
+                                       
+                }
+            }
+
+            //directInputDevice.GetKeyboardUpdates();
+            //directInputDevice.GetKJoystickUpdates();
+
+            
+
+            for (int i = 0; i < searchArray.Length; i++) 
+            {
+                bool ok = XInput.GetState(i, out State state);
+
+                if (ok)
+                {
+                    Vibration vibration = new((ushort)(state.Gamepad.LeftTrigger << 8), (ushort)(state.Gamepad.RightTrigger << 8));
+                    XInput.SetVibration(i, vibration);
+                    searchArray[i] = i;
+                }
+            }
+            
+            Console.Clear();
+
+            if (!searchArray.Where(x => x >= 0).Any())
+            {
+                Console.SetCursorPosition(0, 0);
+
+                ClearLine(); Console.WriteLine($"NO XInput devices available");
+                ClearLine(); Console.WriteLine($"=========================================================================\n");
+
+            }
+            else if (searchArray.Where(x => x >= 0).Any())
+            {
+                ClearLine(); Console.WriteLine($"Available XInput joyskicks is {searchArray.Where(x => x >= 0).Count()}");
+                ClearLine(); Console.WriteLine($"=========================================================================");
+                ClearLine(); Console.WriteLine($"Press 'x' to see if data available\n");
+                
+            }
+            
+            if (directInputDevice.AvailableJoysticks == 0)
+            {
+                ClearLine(); Console.WriteLine($"NO DInput joysticks available");
+                ClearLine(); Console.WriteLine($"=========================================================================");
+            }
+            else if (directInputDevice.AvailableJoysticks >= 0)
+            {
+                ClearLine(); Console.WriteLine($"Available DInput joyskicks is {directInputDevice.AvailableJoysticks}");
+                ClearLine(); Console.WriteLine($"=========================================================================");
+                ClearLine(); Console.WriteLine($"Press 'd' to see if data available");
+            }
+
+
+             Thread.Sleep(10);
         }
 
-        static void Main(string[] args)
+        static void XInputSelect(int[] userIndexArray)
         {
-            int userIndex = 0;
 
             while (true)
             {
@@ -22,39 +103,37 @@ namespace USBWeelToWeb
                     {
                         case 'q':
                             return;
-
-                        case char c when c >= '1' && c <= '4':
-                            userIndex = c - '1';
-                            break;
                     }
                 }
 
-                bool ok = XInput.GetState(userIndex, out State state);
-
-                if (ok)
+                foreach (int userIndex in userIndexArray)
                 {
-                    Vibration vibration = new((ushort)(state.Gamepad.LeftTrigger << 8), (ushort)(state.Gamepad.RightTrigger << 8));
-                    XInput.SetVibration(userIndex, vibration);
+                    bool ok = XInput.GetState(userIndex, out State state);
+
+                    if (ok)
+                    {
+                        Vibration vibration = new((ushort)(state.Gamepad.LeftTrigger << 8), (ushort)(state.Gamepad.RightTrigger << 8));
+                        XInput.SetVibration(userIndex, vibration);
+                    }
+                    else
+                    {
+                        state = new State();    // empty state variable if GetState failed
+                    }
+
+                    Console.SetCursorPosition(0, 0);
+
+                    ClearLine(); Console.WriteLine($"=========================================================================");
+                    ClearLine(); Console.WriteLine($"Information about connected XInput joystick, press 'q' to quit..");
+                    ClearLine(); Console.WriteLine($"=========================================================================");
+                    ClearLine(); Console.WriteLine($"Gamepad       : {userIndex + 1} {(ok ? "(ok)" : "(not ok)")}");
+                    ClearLine(); Console.WriteLine($"Buttons       : {state.Gamepad.Buttons}");
+                    ClearLine(); Console.WriteLine($"Left Thumb    : X = {state.Gamepad.LeftThumbX} Y = {state.Gamepad.LeftThumbY}");
+                    ClearLine(); Console.WriteLine($"Left Trigger  : {state.Gamepad.LeftTrigger}");
+                    ClearLine(); Console.WriteLine($"Right Thumb   : X = {state.Gamepad.RightThumbX} Y = {state.Gamepad.RightThumbY}");
+                    ClearLine(); Console.WriteLine($"Right Trigger : {state.Gamepad.RightTrigger}");
+                    Console.WriteLine();
                 }
-                else
-                {
-                    state = new State();    // empty state variable if GetState failed
-                }
-
-                Console.SetCursorPosition(0, 0);
-
-                ClearLine(); Console.WriteLine($"=========================================================================");
-                ClearLine(); Console.WriteLine($"Press 1-4 to select gamepad, triggers control rumble, press 'q' to quit..");
-                ClearLine(); Console.WriteLine($"=========================================================================");
-                ClearLine(); Console.WriteLine($"Gamepad       : {userIndex + 1} {(ok ? "(ok)" : "(not ok)")}");
-                ClearLine(); Console.WriteLine($"Buttons       : {state.Gamepad.Buttons}");
-                ClearLine(); Console.WriteLine($"Left Thumb    : X = {state.Gamepad.LeftThumbX} Y = {state.Gamepad.LeftThumbY}");
-                ClearLine(); Console.WriteLine($"Left Trigger  : {state.Gamepad.LeftTrigger}");
-                ClearLine(); Console.WriteLine($"Right Thumb   : X = {state.Gamepad.RightThumbX} Y = {state.Gamepad.RightThumbY}");
-                ClearLine(); Console.WriteLine($"Right Trigger : {state.Gamepad.RightTrigger}");
-
-                Thread.Sleep(10);
-            }
+            }                  
         }
     }
 }
